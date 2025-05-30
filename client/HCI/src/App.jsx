d80f08779669a50dbb84da0f995088513420d86b
Obulor Justice <justiceobulor1010@gmail.com>
2025-05-30 07:33:39 +0100

    setup

--- client/HCI/src/App.jsx
+++ client/HCI/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       
         
-      <h1>Vite + React</h1>
+      <h1 className='text-red-400'>Vite + React</h1>
       
       
     </>
--- client/HCI/src/main.jsx
+++ client/HCI/src/main.jsx
@@ -1,4 +1,4 @@
-
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -8,9 +8,10 @@ import MainLayout from './Layouts/MainLayout.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path='' element={ <MainLayout /> }>
-        <Route index element={<App />} />
-      </Route>
+      <Route path='/' element={ <App />} />
+        <Route path='' element={<MainLayout />}>
+
+        </Route>
     </Routes>
    
   </BrowserRouter>
