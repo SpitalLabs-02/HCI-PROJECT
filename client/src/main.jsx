c32e66a097535a5d7f1ee6eb3d0487f8f5f10664
Obulor Justice <justiceobulor1010@gmail.com>
2025-06-02 20:53:28 +0100

    new setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,19 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import MainLayout from './Layouts/MainLayout.jsx'
+import Home from './pages/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={ <App />} />
         <Route path='' element={<MainLayout />}>
-
+          <Route index element={<Home />} />
+          <Route path='/about' />
+          <Route path='/service' />
+          <Route path='/contact' />
+          <Route path='/login' />
+          <Route path='/singup' />
         </Route>
     </Routes>
    
